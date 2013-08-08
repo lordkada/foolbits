80cc98a32fdc1d29b738897ae0b5edcfdc878d85
Carlo Alberto Degli Atti <lordkada@gmail.com>
2013-08-08 17:52:46 +0200

    rails4 doesn't need it

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,5 @@ module Foolbits
     # config.i18n.default_locale = :de
     config.assets.precompile += ["forge/jsbn.js", "forge/aes.js", "forge/asn1", "forge/oids.js", "forge/pkcs1.js", "forge/sha1.js",  "forge/rsa.js", "forge/util.js", 
         "forge/pki.js", "forge/prng.js", "forge/random.js", "forge/pbkdf2.js", "forge/hmac.js", "forge/pem.js", "crypto.js",]
-    config.assets.initialize_on_precompile false
   end
 end
