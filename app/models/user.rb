fd1a6360ab9f6334c10bd0cde91d229e21cf103a
Carlo Alberto Degli Atti <lordkada@gmail.com>
2013-08-15 18:37:42 +0200

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-    before_save { self.email = email.downcase }
+    before_save { self.email = email.downcase if email? }
     before_create :create_remember_token
 
     def User.new_remember_token
