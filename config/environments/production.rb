9ddb7f9f3961ff7ca842ab657965dca9f8307741
Carlo Alberto Degli Atti <lordkada@gmail.com>
2013-09-18 11:23:12 +0200

    images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,8 @@ Foolbits::Application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
 
+  config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
