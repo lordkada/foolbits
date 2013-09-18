e53647d6dc5bba6c087b1fe068d49a6b45ad54c2
Carlo Alberto Degli Atti <lordkada@gmail.com>
2013-09-18 16:19:14 +0200

    fixed routes for url without www

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,24 @@
 Foolbits::Application.routes.draw do
 
-  get '/auth/:provider/callback', to: 'sessions#create'
-  get 'signout', to: 'sessions#destroy'
+    constraints(:host => /^foolbits.com/) do
+        get '/(*path)', to: redirect { |params, request|
+            p request.url
+            URI.parse(request.url).tap { |x| x.host = "www.#{x.host}" }.to_s
+        }
+    end
 
-  get 'user', to: 'sessions#get'
+    get '/auth/:provider/callback', to: 'sessions#create'
+    get 'signout', to: 'sessions#destroy'
 
-  get 'setup', to: "crypto#setup"
-  get 'vault', to: "crypto#vault"
-  post 'keypair', to: "crypto#keypair"
-  put 'vault', to: "crypto#set_vault"
-  
-  get 'faq', to: "faq#index"
-  
-  root 'crypto#index'
+    get 'user', to: 'sessions#get'
+
+    get 'setup', to: "crypto#setup"
+    get 'vault', to: "crypto#vault"
+    post 'keypair', to: "crypto#keypair"
+    put 'vault', to: "crypto#set_vault"
+      
+    get 'faq', to: "faq#index"
+      
+    root 'crypto#index'
 
 end
